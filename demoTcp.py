5affd949bab2631dd97131e77640744e865384c4
adamkaliszan <adam.kaliszan@put.poznan.plcd>
2018-04-25 11:11:35 +0200

    striping the received data

--- demoTcp.py
+++ demoTcp.py
@@ -8,7 +8,7 @@ myBoard = AS.IoTShield(pwmMax=100)
 print("Connection with Arduino estabilished !!!")
 
 TCP_IP = '0.0.0.0'
-TCP_PORT = 5005
+TCP_PORT = 5001
 BUFFER_SIZE = 50  # Normally 1024, but we want fast response
 
 
@@ -17,17 +17,25 @@ s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
 while True:    
+    print ("Waiting for new connection")
     conn, addr = s.accept()
     print ("Connection with client was established")
     
     while True:
-        data = conn.recv(BUFFER_SIZE)
+        data = conn.recv(BUFFER_SIZE).decode()
         if not data: break
     
-        if data == "led1on":
-            myBoard.leds[1].on()
+        data = data.rstrip()    
+    
+        if data == "ping":
+            conn.send("pong".encode())
+    
+        if data == "led2on":
+            myBoard.leds[2].on()
 
-        if data == "led1off":
-            myBoard.leds[1].off()
+        if data == "led2off":
+            myBoard.leds[2].off()
+            
+        print("Received " + data)
             
     conn.close()
